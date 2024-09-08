ed9af76642140b36051e5b44bf51973eec14d76f
zikoo <sidqui.zakaria1@gmail.com>
2024-09-08 12:37:03 +0100

    first Commit

---
+++ react _whit_BroCode/first-try-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My First React web Site  </p>
+        </footer>
+    );
+}
+export default Footer
